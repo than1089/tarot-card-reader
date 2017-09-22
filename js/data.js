231c5892dedc02b0b8f28f2e3438de76c53c265b
Ngo Hoai Than <than1089@gmail.com>
2017-09-22 23:02:03 +0700

    add js css + first page

---
+++ js/data.js
@@ -0,0 +1,27 @@
+var tarots = {
+	1: {
+		img: 'heirophant.png',
+		title: '',
+		content: '',
+	},
+	2: {
+		img: 'heirophant.png',
+		title: '',
+		content: '',
+	},
+	3: {
+		img: 'heirophant.png',
+		title: '',
+		content: '',
+	},
+	4: {
+		title: '',
+		content: '',
+	},
+	5: {
+		img: 'heirophant.png',
+		title: 'The Hierophant',
+		content: `If your birthday adds up to the number 5, The Hierophant represents your lifetime symbol.
+		The Hierophant represents the universal spiritual teacher, the anecdote to the challenging energy represented in the tarot. You are the wise ‘man’, the advisor, the intuitive, manifester of wisdom and spirit. You strongly value family and extended family, are loyal, community-minded, practical.  You are a master at assisting others with inner work and exploration because you are filled with light, awakened and enlightened in this lifetime. Throughout your life you will find that your commitments and pursuits require that mind, heart and action all share the same intention.  Wisdom and courage are yours for the taking, remember they are innately yours. In this lifetime, you will master the art of spirituality and teaching others to find their own personal wisdom and courage.`,
+	}
+}
---
+++ js/tarot.js
@@ -0,0 +1,87 @@
+jQuery(document).ready(function($){
+	$('form#birthdate').submit(function(e){
+		e.preventDefault();
+		var error = false;
+		$(this).find('.form-group').removeClass('has-danger');
+		$(this).find('.form-control-feedback').remove();
+
+		$(this).find('input[type=number]').each(function(){
+			switch(this.id){
+				case 'month':
+					if (!this.value){
+						$(this).parent().addClass('has-danger');
+						$(this).parent().append('<div class="form-control-feedback">This field is required.</div>');
+						error = true;
+						break;
+					}
+					if (this.value.match(/[\D]/) || Number(this.value) > 12 || Number(this.value) < 1){
+						$(this).parent().addClass('has-danger');
+						$(this).parent().append('<div class="form-control-feedback">Invalid month.</div>');
+						error = true;
+					}
+					break;
+				case 'day':
+					if (!this.value){
+						$(this).parent().addClass('has-danger');
+						$(this).parent().append('<div class="form-control-feedback">This field is required.</div>');
+						error = true;
+						break;
+					}
+					if (this.value.match(/[\D]/) || Number(this.value) > 31 || Number(this.value) < 1){
+						$(this).parent().addClass('has-danger');
+						$(this).parent().append('<div class="form-control-feedback">Invalid day.</div>');
+						error = true;
+					}
+					break;
+				case 'year':
+					if (!this.value){
+						$(this).parent().addClass('has-danger');
+						$(this).parent().append('<div class="form-control-feedback">This field is required.</div>');
+						error = true;
+						break;
+					}
+					if (!this.value.match(/^\d{4}$/)){
+						$(this).parent().addClass('has-danger');
+						$(this).parent().append('<div class="form-control-feedback">Invalid year.</div>');
+						error = true;
+					}
+					break;
+			}
+		});
+
+		if (!error){
+			var day   = Number($('#day').val());
+			var month = Number($('#month').val());
+			var year  = Number($('#year').val());
+
+			var total = day+month+year;
+			var number = String(total).split('').reduce(function(accumulator, currentValue) {
+			    return Number(accumulator) + Number(currentValue);
+			});
+
+			console.log(number)
+
+			$('#card').animateCss({
+				animationName: 'rollOut',
+				callback: function () {
+					$('#card').attr('src', 'img/'+tarots[number].img).animateCss({animationName: 'rollIn'});
+				}
+			});
+		}
+
+	});
+})
+$.fn.extend({
+    animateCss: function (options) {
+    	var settings = $.extend({}, {
+    		animationName: 'bounce',
+    		callback: function(){}
+    	}, options );
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        this.addClass('animated ' + settings.animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + settings.animationName);
+            settings.callback();
+        });
+        return this;
+    }
+});
